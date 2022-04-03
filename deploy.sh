c99c4ca26154929512a4c05a4a3d8ff7276adc13
poohRui <yuruil@qq.com>
2022-04-03 23:23:00 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,11 @@ cp -r ftplugin/* ~/.vim/ftplugin
 cp -r autoload/* ~/.vim/autoload
 
 if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
+    echo "Install vim-plug"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+echo "Install plugins"
 vim -c "execute 'silent !cp vimrc.template ~/.vimrc'" -c \
     "execute 'source ~/.vimrc'" -c "PlugInstall" -c "q" -c "q"
